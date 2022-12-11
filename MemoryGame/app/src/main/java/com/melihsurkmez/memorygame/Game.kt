a933303132b1b978e5db041e80b6696d363fff4c
tahapek5454 <tahapek5454@gmail.com>
2022-12-11 16:48:57 +0300

    add basic changeImage operation

--- MemoryGame/app/src/main/java/com/melihsurkmez/memorygame/Game.kt
+++ MemoryGame/app/src/main/java/com/melihsurkmez/memorygame/Game.kt
@@ -3,15 +3,37 @@ package com.melihsurkmez.memorygame
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.widget.ImageButton
 import kotlinx.android.synthetic.main.activity_game.*
 
 class Game : AppCompatActivity() {
+    //Butonlarimizin listesi burada olucak
+    private lateinit var buttons: List<ImageButton>
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
+
+
+        buttons = listOf(imageButton, imageButton2, imageButton3, imageButton4, imageButton5,
+            imageButton6, imageButton7, imageButton8, imageButton9, imageButton10, imageButton11, imageButton12,
+                    imageButton13, imageButton14, imageButton15, imageButton16)
+
+        buttons.forEachIndexed { index, button ->
+            button.setOnClickListener {
+
+                button.setImageResource(R.drawable.ic_baseline_email_24)
+            }
+        }
+
         myTimer()
     }
+
+
+
 
 
     //Timer
